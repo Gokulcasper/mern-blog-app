d091d35ea579e0f8414a886f95bc9908066da6d1
Gokulcasper <ronitcasper215@gmail.com>
2022-09-03 21:35:22 +0530

    Feat : Complete Home page

--- src/posts/Posts.jsx
+++ src/posts/Posts.jsx
@@ -9,6 +9,7 @@ const Posts = () => {
       <Post img="https://images.pexels.com/photos/6711867/pexels-photo-6711867.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" />
       <Post img="https://images.pexels.com/photos/5490778/pexels-photo-5490778.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" />
       <Post img="https://images.pexels.com/photos/4916559/pexels-photo-4916559.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" />
+      <Post img="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" />
     </div>
   );
 };
