c73fbd2ae455962bd24501068bf045e0fd0dbf96
Gokulcasper <ronitcasper215@gmail.com>
2022-09-04 22:41:13 +0530

    Feat : Complete Blog UI

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
-import TopBar from "./components/topbar/TopBar";
+import TopBar from "./components/topbar/Topbar";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
 import Settings from "./pages/settings/Settings";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
+import { BrowserRouter as Router, Routes, Route, } from "react-router-dom";
 
 
 function App() {
+  const user = false;
   return (
-    <div>
+    <Router>
       <TopBar />
-      <Login />
-
-    </div>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/register" element={user ? <Home /> : <Register />} />
+        <Route path="/login" element={user ? <Home /> : <Login />} />
+        <Route path="/write" element={user ? <Write /> : <Register />} />
+        <Route path="/setting" element={user ? <Settings /> : <Register />} />
+        <Route path="/post/:postId" element={<Single />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,57 @@
+// import { Link } from "react-router-dom";
+import "./topbar.css";
+import gokul from "./gokul.png";
+import { Link } from "react-router-dom";
+
+const Topbar = () => {
+  const user = false;
+  return (
+    <div className="top">
+      <div className="topLeft">
+        <i className="topIcon fab fa-facebook-square"></i>
+        <i className="topIcon fab fa-instagram-square"></i>
+        <i className="topIcon fab fa-pinterest-square"></i>
+        <i className="topIcon fab fa-twitter-square"></i>
+      </div>
+      <div className="topCenter">
+        <ul className="topList">
+          <li className="topListItem">
+            <Link className="link" to="/">
+              HOME
+            </Link>
+          </li>
+          <li className="topListItem">ABOUT</li>
+          <li className="topListItem">CONTACT</li>
+          <li className="topListItem">
+            <Link className="link" to="/write">
+              WRITE
+            </Link>
+          </li>
+          <li className="topListItem">{user && "LOGOUT"}</li>
+        </ul>
+      </div>
+      <div className="topRight">
+        {user ? (
+          <img className="topImg" src={gokul} alt="" />
+        ) : (
+          <ul className="topList">
+            <li className="topListItem">
+              <Link className="link" to="/login">
+                LOGIN
+              </Link>
+            </li>
+            <li className="topListItem">
+              <Link className="link" to="/register">
+                REGISTER
+              </Link>
+            </li>
+          </ul>
+        )}
+
+        <i className="topSearchIcon fas fa-search"></i>
+      </div>
+    </div>
+  );
+};
+
+export default Topbar;
