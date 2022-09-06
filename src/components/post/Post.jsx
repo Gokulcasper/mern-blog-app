daa3e79d3dbf3c0bde58f9126590b8b35a382915
Gokulcasper <ronitcasper215@gmail.com>
2022-09-06 18:12:35 +0530

    Feat : add image in multer

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -9,7 +9,7 @@ export default function Post({ post }) {
       <div className="postInfo">
         <div className="postCats">
           {post.categories.map((c) => (
-            <span className="postCat">{c.music}</span>
+            <span className="postCat">{c.name}</span>
           ))}
         </div>
         <Link to={`/post/${post._id}`} className="link">
